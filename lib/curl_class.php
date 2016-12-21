237a7a2dba60bff694f7c40d13f62fe2f2c1a17b
Alexandr <admin@dp-malish.com>
2016-12-21 17:28:17 +0200

    ++

---
+++ lib/curl_class.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: WinTeh
+ * Date: 21.12.2016
+ * Time: 14:15
+ */
+class Curl{
+
+    public $url=null;
+    public $cookie='/cookie.txt';
+
+
+    public function __construct(){
+        //если требуется авторизация то нужно задать эти параметры при создании обьетк
+        $this->url			=		$options['url']; //только url сайта
+        $this->cookie_patch = 		$options['cookie_patch']."/cookie.txt";	//путь куда будем сохранять куки для авторизации
+        return $this;
+    }
+    
+    private conne
+
+}
---
+++ www/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 21.12.2016
+ */
+
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Документ</title>
+</head>
+
+<body>
+<form action="" method="post">
+    <input type="text" name="url" value="http://dp-malish">
+    <input type="submit">
+</form>
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 21.12.2016
+ */
+
+
+?>
+</body>
+</html>
